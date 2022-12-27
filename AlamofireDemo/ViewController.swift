cd525d032458bb9daf46b1f09fd26f5ba717e916
Mustofa Mahmud <mustofa.mahmud@bjitgroup.com>
2022-12-27 15:17:58 +0600

    Alamofire demo

--- AlamofireDemo/ViewController.swift
+++ AlamofireDemo/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        AF.request("https://reactnative.dev/movies.json")
+            .responseJSON { response in
+              //  print(response)
+               let data = response.data
+                do{
+                    let result = try JSONDecoder().decode(MovieObject.self, from: data!)
+                    if(result != nil){
+                        print(result.title)
+                        print(result.movies.count)
+                    }
+                }catch{
+                    print("err: \(error)")
+                }
+                
+                
+            }
     }
 
+}
 
+class MovieObject: Codable{
+    let title: String!
+    let description: String!
+    let movies: [Movie]!
+}
+
+class Movie: Codable{
+    let id: String!
+    let title: String!
+    let releaseYear: String!
 }
 
